2bae22bbc4025c5a5fadc4cf2540161a89a499f7
Eugene Khorev <eugene.khorev@gmail.com>
2016-11-26 13:12:09 +0300

    Schedule model validation rules

--- models/Schedule.php
+++ models/Schedule.php
@@ -4,5 +4,15 @@ namespace app\models;
 
 class Schedule extends \yii\db\ActiveRecord
 {
-    
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'doctor_id', 'reserved_from', 'reserved_till'], 'required'],
+        ];
+    }
+
 }
