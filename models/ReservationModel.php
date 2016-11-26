b48868260f24031c8426a3829ecb3df8544f258f
Eugene Khorev <eugene.khorev@gmail.com>
2016-11-26 13:11:38 +0300

    Reservation model

---
+++ models/ReservationModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+
+class ReservationModel extends Model
+{
+
+    public $doctorId;
+    public $reservationTime;
+
+    public function rules()
+    {
+        return [
+            [['doctorId', 'reservationTime'], 'required'],
+            ['doctorId', 'integer'],
+            ['reservationTime', 'datetime', 'format' => 'php:Y-m-d H:i:s'],
+        ];
+    }
+
+}
